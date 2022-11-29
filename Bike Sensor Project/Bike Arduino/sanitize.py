2ba5ea163891db8db460a4f0e3c89564979a6460
jmmaweu <112222143+jmmaweu@users.noreply.github.com>
2022-11-29 12:20:43 -0600

    Update sanitize.py

--- Bike Sensor Project/Bike Arduino/sanitize.py
+++ Bike Sensor Project/Bike Arduino/sanitize.py
@@ -1,39 +1,21 @@
 # sanitize: RX bike rack serial data cooked and pushed to Rack objects
 #   per ME396P-F2022-G03/Standards/board-data-flow.jpeg
-# 
-# revision
-#   2022-11-06: v1, initial
-#     not much here, but notes and stubs
-#     rack dump format is TBD, but using parser-generator avoids some redevelopment
-# 
-# overview:
-#   * called somehow during initialization of python side of Bike Arduino
-#       all rack dump windows reset
-#   * if called with port_baud_list=None, then
-#       goes through available comports and datarates listening for meaningful data
-#   * available comports determined by one of the methods in
-#       https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
-#   * datarates are the "standard values" listed in
-#       https://pyserial.readthedocs.io/en/latest/pyserial_api.html
-#   * meaningful data is an entire rack dump (in https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form)
-#       each rack has some number (>=1) of bays
-#       each bay has some number (>=1) of sensors
-#       rack dump format is TBD
-#   * rack dump data fills a rack dump window (one per rack)
-#   * when rack dump window is full
-#       new rack dump causes oldest rack dump to leave window
-#       rack dump window is classified for each bay
-#       classifier results sent to rack object via update_rack
+#   per "<'Available '> + <distance> <'\n'>" in Bike Arduino/ESP_BT_Handler.py
+#   per Bike Arduino/BT_with_info_string_ESP32/BT_with_info_string_ESP32.ino
+#   per "Eg_dict = {0 : True, 1: False, 2 : True, 3 : True, 4 : False}" in Standards/Rack_Dict_convention
+#   per Bike Arduino/mod_example_sonar_arduino_uno_version/mod_example_sonar_arduino_uno_version.ino
 
 from ESP_BT_Handler import *
 from Rack import *
 #import Rack as Rack
 import re
 
+line_re = None
+classifier_data = None
 rack = None
 
 def load_classifier_data():
-    pass
+    classifier_data = {'CUTOFF_ONE: 30,'CUTOFF_TWO': 10}
 
 def rack_init(rack_map_data):
     rack = Rack()
@@ -41,30 +23,31 @@ def rack_init(rack_map_data):
 
 def sanitize(rack_map_data, port_baud_list=None):
     def parse_line(line):
-        pass
+        return line_re.match(line)
     
-    def grammar_is_good(ast):
-        pass
+    def grammar_is_good(line_data):
+        return bool(line_data)
    
-    def classify(ast):
-        ''' return t/f '''
-        #return the_dict
+    def classify(line_data):
+        return {0 : line_data.match(1) == 'AVAILABLE'}
     
     def push_to_rack(the_dict):
         rack.update(the_dict)
         pass
     
     def port_listnener(port):
-        get_messages(port = 7)  ##### udpate port if necessary (unlikely)
-        ast = parse_line(line)
-        if grammar_is_good(ast):
-            the_dict = classify(ast)
+        line = get_messages(port = 7)  ##### udpate port if necessary (unlikely)
+        line_data = parse_line(line)
+        if grammar_is_good(line_data):
+            the_dict = classify(line_data)
             push_to_rack(the_dict)
     
     port_listener()
 
 def supervise():
     load_classifier_data()
+    distance_pat = '(0|[0-9][1-9]*)'
+    line_re = re.compile(r'(AVAILABLE|NOT AVAILABLE) ' + distance_pat + '\n')
     rack_init(rack_map_data)
     while True:
         sanitize()
